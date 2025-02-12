056f8fc818f2a2c775d12b8ba58581e57135dad2
Thorpad <thorpad.boo@student.mahidol.edu>
2025-02-12 08:38:55 +0000

    add 3 files

--- Node.h
+++ Node.h
@@ -21,6 +21,11 @@ void enqueue(NodePtr * head, NodePtr* tail, int x){
   Node* new_node=(NodePtr) malloc(sizeof(Node));
 if(new_node){ 
     /* Finish queue*/
+    new_node->data = x;
+    new_node->nextPtr=NULL;
+    if(*head==NULL) *head=new_node;
+    else (*tail)->nextPtr=new_node;
+    *tail=new_node;
  }
 }
 
@@ -30,6 +35,8 @@ int dequeue(NodePtr* head, NodePtr* tail){
    if(t){
    int value= t->data;
    /* Finish dequeue*/
+   *head = t->nextPtr;
+   if(*head==NULL) *tail=NULL;
        
        
    return value;
